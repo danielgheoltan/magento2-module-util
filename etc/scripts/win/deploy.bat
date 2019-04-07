32d113e559658467dd656553fa1480d1cd803af2
Daniel Gheoltan <danielgheoltan@gmail.com>
2019-04-07 23:13:17 +0300

    Update deploy.bat

--- etc/scripts/win/deploy.bat
+++ etc/scripts/win/deploy.bat
@@ -8,22 +8,20 @@ CALL php bin/magento maintenance:enable
 
 :: Cleanup
 RMDIR /S /Q "generated" "pub/static/adminhtml" "pub/static/frontend" "var/cache" "var/page_cache" "var/view_preprocessed" 2>NUL
+RMDIR /S /Q "pub/media/catalog/product/cache"
 DEL /Q /F "pub\static\deployed_version.txt" 2>NUL
-
-:: Flush cache
-CALL php bin/magento cache:flush
 
 :: Update required components
 CALL composer update
+
+:: Flush cache
+CALL php bin/magento cache:flush
 
 :: Run setup scripts
 CALL php bin/magento setup:upgrade
 
 :: Reindex
 CALL php bin/magento indexer:reindex
-
-:: Resize images
-:: CALL php bin/magento catalog:images:resize
 
 :: Deploy static view files
 CALL php bin/magento setup:static-content:deploy en_US --no-html-minify -f
